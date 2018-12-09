2c01339f5610c4f24e693eeb3de85bbc6edd6fa0
BuildTools <unconfigured@null.spigotmc.org>
2018-12-09 23:19:28 +0800

    V0.4

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@ function background(n)
     var randomnum = Math.random();
     var randomnum = Math.ceil(n* randomnum);
     bg = document.getElementById("bg");
-    bg.setAttribute("style","background: url(../BlogV2.0/img/Pic" + randomnum+ ".png) no-repeat center;background-size: cover;animation: fade-out 0.7s ease-out;");
+    bg.setAttribute("style","background: url(../img/Pic" + randomnum+ ".png) no-repeat center;background-size: cover;animation: fade-out 0.7s ease-out;");
 }
 
 window.onload = function()
