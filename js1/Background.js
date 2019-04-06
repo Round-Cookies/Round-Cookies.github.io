c180b2c6765ceb045b8ec90283ecd1bd0e419df0
Round-Cookies <Cookies_201013@126.com>
2019-04-06 15:18:31 +0800

    Used PhotoBoost

--- js1/Background.js
+++ js1/Background.js
@@ -14,7 +14,23 @@ function BackgroundChange()
 function Settimer()
 {
     document.getElementById("bg").className= "background";
-    document.getElementById("bg").style.background = 'url("img1/Background' + num + '.jpg") no-repeat center';
+    if(num==0)
+    {
+        addr="https://i.loli.net/2019/04/06/5ca850c948541.jpg";
+    }
+    if(num==1)
+    {
+        addr="https://i.loli.net/2019/04/06/5ca850c956e4b.jpg";
+    }
+    if(num==2)
+    {
+        addr="https://i.loli.net/2019/04/06/5ca850c95329a.jpg";
+    }
+    if(num==3)
+    {
+        addr="https://i.loli.net/2019/04/06/5ca850c95b194.jpg";
+    }
+    document.getElementById("bg").style.background = 'url('+ addr +') no-repeat center';
     document.getElementById("bg").style.backgroundSize = 'cover';
     var t=setTimeout("BackgroundChange()",5000);
 }
